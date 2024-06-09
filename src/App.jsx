429304ef386ce4b2ad8055cab256a21ce503c595
Yassin Benyaiche <yassinbenyaiche@gmail.com>
2024-06-09 13:34:07 +0100

    correccion2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/Home.jsx";
 import PassContextProvider from "./context/Pass.context.jsx";
 import { Routes, Route } from "react-router-dom";
-import PassGenerator from "./pages/Passgenerator.jsx";
+import PassGenerator from "./pages/PassGenerator.jsx";
 import MainLayout from "./layout/MainLayout.jsx";
 import KeepedPasswords from "./pages/KeepedPasswords.jsx";
 const App = () => {
